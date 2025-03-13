05755e8af3aa0ad448b6631e1b5ac80a6d945576
Ramon Klanke <rklanke@schubergphilis.com>
2025-03-13 10:59:28 +0100

    fix: offset icon

--- src/components/FlowNodeIcon.tsx
+++ src/components/FlowNodeIcon.tsx
@@ -8,7 +8,7 @@ interface Props {
 }
 
 const FlowNodeIcon = ({ children, boxWidth, boxHeight }: Props) => {
-	const offset = 10
+	const offset = Math.max(boxHeight / 4, 5)
 	return (
 		<Container transform={`translate(${boxWidth / 2 - boxHeight / 2} , 0)`}>
 			<svg x={offset} y={offset} width={boxHeight - offset * 2} height={boxHeight - offset * 2}>
